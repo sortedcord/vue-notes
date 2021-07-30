601c9e880ceb9481430fba45620d4003b514bdf3
sortedcord <divyashgupta2005@gmail.com>
2021-07-30 11:49:26 +0530

    Last Sync: 2021-07-30 11:49:25

--- Project Files/2.1 basics-01-starting-code/app.js
+++ Project Files/2.1 basics-01-starting-code/app.js
@@ -1,10 +1,9 @@
 const app = Vue.createApp({
   data() {
     return {
-        courseGoal: 'Finish the course and lern vue!',
-        courseLink : 'https://google.com'
+      courseGoal: "Finish the course and lern vue!",
+      courseLink: "https://google.com",
     };
   },
 });
-
 app.mount("#user-goal");
